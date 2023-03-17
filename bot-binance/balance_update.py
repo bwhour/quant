26fdb4340b55eda6540f5a1ddfceb0238bb0f625
bwhour <juvemeen.xio@gmail.com>
2023-03-17 21:27:52 +0800

    delete files

--- bot-binance/balance_update.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class BalanceUpdate:
-    asset: str
-    balance_delta: float
-    event_time: int
-    clear_time: int
--- bot-binance/scripts/check-bot-service.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SERVICE_NAME=binance-dynamic-savings-bot.service
-
-sudo systemctl status ${SERVICE_NAME}
